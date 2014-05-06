56deb9f58f64402a7224cadd6c7e681ed71ec581
ryonagana <nicholasluis@gmail.com>
2014-05-05 22:27:41 -0300

    Converting to OOP

--- utils.py
+++ utils.py
@@ -6,3 +6,8 @@ def format(data): #all irc messages must carry \r\n  and received data comes wit
 	return "{0}\r\n".format(data)
 
 
+def sanitize(data):
+	if(data.find("\r") or data.find('\n')):
+		return data.split("\r\n")
+	elif(data.find('\r\n')):
+		return return data.split("\r\n")
